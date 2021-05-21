7443e21140dc0bafe7cfb244c9c6d08ddf93adb9
brucegrapes <govardanan.gms@gmail.com>
2021-05-21 12:29:59 +0530

    Bug fixes

--- src/components/EditUser/EditUserForm.js
+++ src/components/EditUser/EditUserForm.js
@@ -1,11 +1,12 @@
 import { Avatar, Grid } from '@material-ui/core';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {useForm,Form} from '../useForm';
 import Input from '../Form/Input';
 import Button from '../Form/Button';
 import { Alert } from '@material-ui/lab';
 import { fetchData } from '../../Api/FetchData';
 import Loader from '../UI/Loader';
+import { customTimeout } from '../../Utils/CustomTimeout';
 
 
 function EditUserForm({initialValues,updateData,data}) {
@@ -13,6 +14,7 @@ function EditUserForm({initialValues,updateData,data}) {
     const [isPending , setIsPending] = useState(false);
     const [isError,setIsError] = useState(null);
     const [successText,setSuccessText] = useState(null);
+    let Ttimer;
     const validate = () =>{
         let temp = {}
         temp.name = formData.first_name?"":"Required"
@@ -20,11 +22,15 @@ function EditUserForm({initialValues,updateData,data}) {
         setError({...temp});
         return Object.values(temp).every(x => x==="");
     }
+    useEffect(() => {
+        return () => {
+            clearInterval(Ttimer);
+        }
+    })
     const handleSubmit = (e) =>{
         e.preventDefault();
         if(validate()){
             const post = { id:formData.id, email:formData.email,first_name:formData.first_name, last_name:formData.last_name, avatar:formData.avatar  };
-            console.log(JSON.stringify(post));
             setIsPending(true);
             var createPostHeader = new Headers();
             createPostHeader.append("Content-Type", "application/json");
@@ -34,11 +40,10 @@ function EditUserForm({initialValues,updateData,data}) {
                 body : JSON.stringify(post)
             };
             let url = "https://reqres.in/api/users/"+formData.id;
+            Ttimer = ()=> customTimeout(3000,()=>setSuccessText(null));
             fetchData(url,requestOptions,setIsError)
             .then(res=>{
-                setSuccessText(`${post.first_name} ${post.last_name} Updated Successfully`,setTimeout(() => {
-                    setSuccessText(null);
-                }, 3000))
+                setSuccessText(`${post.first_name} ${post.last_name} Updated Successfully`,Ttimer());
                 setIsPending(false);
                 let newdata  = data.filter((item)=>item.id!==formData.id)
                 newdata.push(post);
